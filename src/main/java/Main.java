6ad9eede8d99dd1bde9f02c1d8e918c4c99edd0a
a-moseman <drewmoseman@gmail.com>
2022-04-17 16:19:52 -0400

    Implement CSV.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        String[][] input = {{"cat", "shark", "cactus"}, {"cat", "shark", "cactus"}};
+        byte[] bytes = CSV.compress(input);
+        System.out.println(CSV.uncompress(bytes));
     }
 }
